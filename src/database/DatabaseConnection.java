d0527f9cf457cf32eb5cbfd58c1ca69a3ef8b3c6
Saurav Shrestha <sauravshrestha2005@gmail.com>
2025-02-03 12:56:58 +0545

    Database Connection changed - Database connect logic done

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -3,18 +3,29 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/Quiz_Game";
+    private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String USER = "root";
     private static final String PASSWORD = "Password_mysql10169";
+    private static final String DB_NAME = "Quiz_Game";
     
     private static Connection connection;
 
     public static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+                Connection tempConnection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+                try (Statement statement = tempConnection.createStatement()) {
+                    statement.executeUpdate("CREATE DATABASE IF NOT EXISTS " + DB_NAME);
+                }
+                
+                tempConnection.close();
+
+                connection = DriverManager.getConnection(URL + DB_NAME, USER, PASSWORD);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -26,7 +37,7 @@ public class DatabaseConnection {
         try {
             if (connection != null && !connection.isClosed()) {
                 connection.close();
-                System.out.println("Database connection closed.");
+//                System.out.println("Database connection closed.");
             }
         } catch (SQLException e) {
             System.err.println("Error closing the connection: " + e.getMessage());
